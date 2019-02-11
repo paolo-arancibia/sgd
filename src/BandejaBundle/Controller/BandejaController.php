6adca5f879721cc2482f36becf81d5622208ac32
Paolo Arancibia <paolo.arancibia@gmail.com>
2019-02-11 16:28:13 +0000

    nuevos cambios para derivarForm

--- src/BandejaBundle/Controller/BandejaController.php
+++ src/BandejaBundle/Controller/BandejaController.php
@@ -8,6 +8,7 @@ use BandejaBundle\Entity\TiposDocumentos;
 // use BandejaBundle\Form\DerivarType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SearchType;
@@ -32,20 +33,6 @@ class BandejaController extends Controller
                     ])
                     ->getForm();
 
-        /*
-        $filtersForm = $this->createFormBuilder()
-
-                     ->add('clases', ChoiceType::class, [
-                         'choices' => [
-                             'Pendientes' => 'pendientes',
-                             'Archivados' => 'archivados',
-                         ],
-                         'choices_as_values' => true,
-                         'expanded' => true,
-                         'multiple' => false,
-                         'label_attr' => ['class' => 'form-check-label px-2 py-1'],
-                     ])
-                     ->getForm();*/
         $derivarForm = $this->getDerivarForm();
 
         return $this->render(
@@ -140,53 +127,52 @@ class BandejaController extends Controller
     {
         $deptos = $this->getDeptos();
 
-        $derivarForm = $this->createFormBuilder()
-                     ->add('originales', ChoiceType::class, [
-                         'choices'=> $deptos,
-                         'choices_as_values' => true,
-                         'expanded' => false,
-                         'multiple' => true,
+        return $this->createFormBuilder()
+            ->add('originales', ChoiceType::class, [
+                'choices'=> $deptos,
+                'choices_as_values' => true,
+                'expanded' => false,
+                'multiple' => true,
+                'label_attr' => ['class' => 'form-label mt-1'],
+                'attr' => ['class' => 'chosen-select'],
+                'label' => 'Enviar Originales a',
+                'choice_value' => function (Departamentos $depto = null) {
+                    return $depto ? $depto->getIdDepartamento() : '';
+                },
+                'choice_label' => function (Departamentos $depto = null) {
+                    return $depto ? $depto->getDescripcion() : '';
+                }
+            ])->add('nota-original', TextareaType::class, [
+                'label' => 'Nota para originales',
+                'label_attr' => ['class' => 'form-label mt-2'],
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('copias', ChoiceType::class, [
+                'choices'=> $deptos,
+                'choices_as_values' => true,
+                'expanded' => false,
+                'multiple' => true,
                          'label_attr' => ['class' => 'form-label mt-1'],
-                         'attr' => ['class' => 'chosen-select'],
-                         'label' => 'Enviar Originales a',
-                         'choice_value' => function (Departamentos $depto = null) {
-                             return $depto ? $depto->getIdDepartamento() : '';
-                         },
-                         'choice_label' => function (Departamentos $depto = null) {
-                             return $depto ? $depto->getDescripcion() : '';
-                         }
-                     ])->add('nota-original', TextareaType::class, [
-                         'label' => 'Nota para originales',
-                         'label_attr' => ['class' => 'form-label mt-2'],
-                         'attr' => ['class' => 'form-control']
-                     ])
-                     ->add('copias', ChoiceType::class, [
-                         'choices'=> $deptos,
-                         'choices_as_values' => true,
-                         'expanded' => false,
-                         'multiple' => true,
-                         'label_attr' => ['class' => 'form-label mt-1'],
-                         'attr' => ['class' => 'chosen-select'],
-                         'label' => 'Enviar Copias a',
-                         'choice_value' => function (Departamentos $depto = null) {
-                             return $depto ? $depto->getIdDepartamento() : '';
-                         },
-                         'choice_label' => function (Departamentos $depto = null) {
-                             return $depto ? $depto->getDescripcion() : '';
-                         }
-                     ])->add('nota-copias', TextareaType::class, [
-                         'label' => 'Nota para copias',
-                         'label_attr' => ['class' => 'form-label mt-2'],
-                         'attr' => ['class' => 'form-control']
-                     ])->add('adjuntos', FileType::class, [
-                         'required' => false,
-                         'multiple' => true,
-                         'attr' => ['class' => 'adjuntos'],
-                         'label_attr' => ['class' => 'form-label mt-2'],
-                         'label' => 'Adjuntos',
-                     ])
-                     ->getForm();
-        return $derivarForm;
+                'attr' => ['class' => 'chosen-select'],
+                'label' => 'Enviar Copias a',
+                'choice_value' => function (Departamentos $depto = null) {
+                    return $depto ? $depto->getIdDepartamento() : '';
+                },
+                'choice_label' => function (Departamentos $depto = null) {
+                    return $depto ? $depto->getDescripcion() : '';
+                }
+            ])->add('nota-copias', TextareaType::class, [
+                'label' => 'Nota para copias',
+                'label_attr' => ['class' => 'form-label mt-2'],
+                'attr' => ['class' => 'form-control']
+            ])->add('adjuntos', FileType::class, [
+                'required' => false,
+                'multiple' => true,
+                'attr' => ['class' => 'adjuntos'],
+                'label_attr' => ['class' => 'form-label mt-2'],
+                'label' => 'Adjuntos',
+            ])
+            ->getForm();
     }
 
     private function getNuevoForm()
@@ -194,28 +180,65 @@ class BandejaController extends Controller
         $tiposDocs = $this->getTiposDocs();
 
         $doc = new Documentos();
-        $nuevoForm = $this->createFormBuilder($doc)
-                   ->add('fkTipoDoc', ChoiceType::class, [
-                       'choices'=> $tiposDocs,
-                       'choices_as_values' => true,
-                       'expanded' => false,
-                       'multiple' => false,
-                       'attr' => ['class' => 'chosen-select'],
-                       'label_attr' => ['class' => ''],
-                       'label' => 'Tipo Documento',
-                       'choice_value' => function (TiposDocumentos $td = null) {
-                           return $td ? $td->getIdTiposDoc() : '';
-                       },
-                       'choice_label' => function (TiposDocumentos $td = null) {
-                           return $td ? $td->getAbrev() . ' - ' . $td->getDescripcion() : '';
-                       }
-                   ])->add('nroExpediente', NumberType::class, [
-                       'attr' => ['class' => 'chosen-select'],
-                       'label_attr' => ['class' => ''],
-                       'label' => 'Número Expediente',
-                   ])
-                   ->getForm();
 
-        return $nuevoForm;
+        return $this->createFormBuilder($doc)
+            //->add('')
+            ->add('fkTipoDoc', ChoiceType::class, [
+                'choices'=> $tiposDocs,
+                'choices_as_values' => true,
+                'expanded' => false,
+                'multiple' => false,
+                'attr' => ['class' => 'chosen-select'],
+                'label_attr' => ['class' => 'mb-0'],
+                'label' => 'Tipo Documento',
+                'choice_value' => function (TiposDocumentos $td = null) {
+                    return $td ? $td->getIdTiposDoc() : '';
+                },
+                'choice_label' => function (TiposDocumentos $td = null) {
+                    return $td ? $td->getAbrev() . ' - ' . $td->getDescripcion() : '';
+                }
+            ])->add('nroExpediente', NumberType::class, [
+                'attr' => ['class' => 'form-control'],
+                'label_attr' => ['class' => 'mb-0'],
+                'label' => 'Número Expediente',
+                'required' => false,
+            ])->add('fechaDoc', DateType::class, [
+                'attr' => ['class' => 'form-control'],
+                'label_attr' => ['class' => 'mb-0'],
+                'label' => 'Fecha Recepción',
+                'widget' => 'single_text',
+                'required' => true,
+            ])->add('ant', TextareaType::class, [
+                'attr' => ['class' => 'form-control', 'rows' => 4],
+                'label_attr' => ['class' => 'mb-0'],
+                'label' => 'Antecedentes',
+                'required' => true,
+            ])->add('mat', TextareaType::class, [
+                'attr' => ['class' => 'form-control', 'rows' => 4],
+                'label_attr' => ['class' => 'mb-0'],
+                'label' => 'Materia',
+                'required' => true,
+            ])->add('ext', TextareaType::class, [
+                'attr' => ['class' => 'form-control', 'rows' => 4],
+                'label_attr' => ['class' => 'mb-0'],
+                'label' => 'Extracto',
+                'required' => true,
+            ])
+            ->getForm();
     }
+
+    /*
+      $filtersForm = $this->createFormBuilder()
+
+      ->add('clases', ChoiceType::class, [
+      'choices' => [
+      'Pendientes' => 'pendientes',
+      'Archivados' => 'archivados',
+      ],
+      'choices_as_values' => true,
+      'expanded' => true,
+      'multiple' => false,
+      'label_attr' => ['class' => 'form-check-label px-2 py-1'],
+      ])
+      ->getForm();*/
 }
